60db8df9d6c55b601cbee42e836083e7a2d9b0e6
kerneyJ <gamerguy9801@gmail.com>
2023-01-26 10:17:50 -0600

    added test create task method

--- test.py
+++ test.py
@@ -7,7 +7,16 @@ def test_init_dfk():
     print(cdfk.info_dfk())
 
 def test_create_task():
-    pass
+    import time
+    cdfk.init_dfk(10)
+    cdfk.submit("htex", "add", time.time(), False, object(), object(), object(), object(), object())# , object(), object(), object(), object())
+    print(cdfk.info_task(0))
 
 if __name__ == "__main__":
+    print("Testing init dfk")
     test_init_dfk()
+    print("test concluded")
+
+    print("Testing create task and info task")
+    test_create_task()
+    print("test concluded")
