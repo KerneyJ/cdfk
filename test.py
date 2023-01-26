57ebb5f0c725d894fc3ee06f20f2e2f5c918d33e
kerneyJ <gamerguy9801@gmail.com>
2023-01-26 08:15:46 -0600

    made test.py clean

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 #!../cdfkenv/bin/python3
 import cdfk
 
-print(cdfk.info_dfk())
-cdfk.init_dfk(10)
-print(cdfk.info_dfk())
+def test_init_dfk():
+    print(cdfk.info_dfk())
+    cdfk.init_dfk(10)
+    print(cdfk.info_dfk())
+
+def test_create_task():
+    pass
+
+if __name__ == "__main__":
+    test_init_dfk()
