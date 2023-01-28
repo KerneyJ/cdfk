3ede61a2483ca6fb946d6ffd8dc2986832875b55
kerneyJ <gamerguy9801@gmail.com>
2023-01-28 17:08:20 -0600

    made test.py use unittest

--- test.py
+++ test.py
@@ -1,43 +1,35 @@
 #!../cdfkenv/bin/python3
 import cdfk
+import unittest
 
-def test_init_dfk():
-    print(cdfk.info_dfk())
-    cdfk.init_dfk(10)
-    print(cdfk.info_dfk())
+class TestCDFK(unittest.TestCase):
 
-def test_invoke_exec_submit():
-    # Uses XQExecutor because this executor does not have a notion
-    # of a provider. Was never sure why it was so challenging to run
-    # the executor detached from the DFk but it seemed as though it
-    # was the provider that made stuff not
-    import time
+    def test_init_dfk(self):
+        cdfk.init_dfk(10)
 
-    from parsl.executors import XQExecutor
+    def test_invoke_exec_submit(self):
+        # Uses XQExecutor because this executor does not have a notion
+        # of a provider. Was never sure why it was so challenging to run
+        # the executor detached from the DFk but it seemed as though it
+        # was the provider that made stuff not
+        import time
+        from uuid import uuid4
+        from parsl.executors import XQExecutor
 
-    executor = XQExecutor(
-        max_workers=int(4),
-    )
-    cdfk.init_dfk(10)
+        executor = XQExecutor(
+            max_workers=int(4),
+        )
+        executor.run_dir = f"parsl_run_dir/{str(uuid4())}"
+        cdfk.init_dfk(10)
 
-    def add():
-        return 1 + 3;
+        def add():
+            return 1 + 3;
 
-    executor.start()
-    exec_fu = cdfk.submit("xq", "add", time.time(), False, object(), executor, add)
-    # executor.submit(add, None)
-    print(cdfk.info_task(0))
-    while not exec_fu.done():
-        pass
-    print(exec_fu)
-    executor.shutdown()
+        executor.start()
+        exec_fu = cdfk.submit("xq", "add", time.time(), False, object(), executor, add)
+        while not exec_fu.done():
+            pass
+        executor.shutdown()
 
 if __name__ == "__main__":
-    print("Testing init dfk")
-    test_init_dfk()
-    print("test concluded")
-
-    print("Test invoke executor submit on real executor")
-    test_invoke_exec_submit()
-    print("test concluded")
-
+    unittest.main(verbosity=2)
