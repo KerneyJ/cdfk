282eb02ab0b59a80a8c88005e8d3a73a51741421
kerneyJ <gamerguy9801@gmail.com>
2023-01-31 20:07:55 -0600

    moved a bunch of stuff

--- cdfk-pkg/bind.c
+++ cdfk-pkg/bind.c
@@ -1,6 +1,5 @@
 #include "dflow.h"
 
-char method_docs[] = "this is a test method";
 char init_dfk_docs[] = "The method will initialize the dfk. In doing so this method will allocate memory for the dag, and set global variables necessary for the dag.";
 char dest_dfk_docs[] = "The method will destroy to dfk. In doing so this method will deallocate memory for the dag and reset global state.";
 char info_dfk_docs[] = "The method prints the global state associated with the dfk.";
@@ -9,7 +8,6 @@ char submit_docs[] = "TODO Implement. Will be responsible for submiting task to 
 char info_task_docs[] = "Takes as input an id as an int and returns the information about that task";
 
 PyMethodDef cdfk_funcs[] = {
-    {"method", (PyCFunction)method, METH_VARARGS, method_docs},
     {"init_dfk", (PyCFunction)init_dfk, METH_VARARGS, init_dfk_docs},
     {"dest_dfk", (PyCFunction)dest_dfk, METH_NOARGS, dest_dfk_docs},
     {"info_dfk", (PyCFunction)info_dfk, METH_NOARGS, info_dfk_docs},
