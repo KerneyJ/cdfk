f130659f9e321c61c78c141a98433243d4c1280d
kerneyJ <gamerguy9801@gmail.com>
2023-02-17 17:10:13 -0600

    added test python app

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 import cdfk
 import unittest
 
+from parsl import python_app
+
 """
     def test_init_dfk(self):
         cdfk.init_dfk(10)
@@ -31,14 +33,14 @@ import unittest
         executor.shutdown()
 """
 
-class TestFastDFK(unittest.TestCase):
+class TestCDFK(unittest.TestCase):
     run_dir = "parsl_run_dir/"
     def test_internal_executor(self):
         def add():
             return 1 + 3
 
         executor = cdfk.intrnlexec.FDFKInternalExecutor(worker_count=4)
-        executor.run_dir = TestFastDFK.run_dir
+        executor.run_dir = TestCDFK.run_dir
         executor.start()
         exec_fu = executor.submit(add)
         while not exec_fu.done():
@@ -52,7 +54,7 @@ class TestFastDFK(unittest.TestCase):
             return 1 + 3
         cdflow.init_dfk(10)
         executor = cdfk.intrnlexec.FDFKInternalExecutor(worker_count=4)
-        executor.run_dir = TestFastDFK.run_dir
+        executor.run_dir = TestCDFK.run_dir
         executor.start()
         cdflow.add_executor_dfk(executor, executor.label)
         exec_fu = cdflow.submit("add", time.time(), False, object(), add)
@@ -61,22 +63,26 @@ class TestFastDFK(unittest.TestCase):
         cdflow.dest_dfk()
         executor.shutdown()
 
-    def test_py_dfk_exec(self):
-        dfk = cdfk.dflow.FastDFK()
-        dfk.info_executors()
-
     def test_py_dfk_wrapper(self):
         def add():
             return 1 + 3
 
-        dfk = cdfk.dflow.FastDFK()
+        dfk = cdfk.dflow.DataflowKernel()
         exec_fu = dfk.submit(add, None, join=False)
         while not exec_fu.done():
             pass
         dfk.cleanup()
 
-    def test_dflow(self):
-        pass
+    def test_python_app(self):
+        dfk = cdfk.dflow.DataflowKernel()
+        @python_app(data_flow_kernel=dfk)
+        def add():
+            return 1 + 3
+
+        num = add()
+        while not num.done():
+            pass
+        dfk.cleanup()
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
