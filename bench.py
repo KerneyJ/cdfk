b3a898b1d6fa7e497b8762622ace233b80aa4de0
kerneyJ <gamerguy9801@gmail.com>
2023-02-17 18:52:23 -0600

    removed comments from test.py; benchmark dfk

--- bench.py
+++ bench.py
@@ -1,38 +1,39 @@
 #!venv/bin/python3
 
 import cdfk
-import parsl
 import time
 import sys
 
+from parsl.config import Config
 from parsl.executors import XQExecutor
+from parsl import python_app
 from uuid import uuid4
+
+xq_config = Config(
+        executors=[XQExecutor(
+            max_workers=8
+        )],
+        run_dir="runinfo"
+)
+
+dfk = cdfk.dflow.DataflowKernel(config=xq_config)
+@python_app(data_flow_kernel=dfk)
+def add():
+    return 2 + 2
 
 def bench_50k_noop_dfk():
     pass
 
-def bench_50k_noop_cdfk(worker_count):
-    def noop():
-        import time
-        time.sleep(0)
-        return None
-    noop_count = 50000
-    executor = XQExecutor(
-        max_workers=worker_count,
-    )
-    executor.run_dir = f"parsl_run_dir/{str(uuid4())}"
-    cdfk.init_dfk(noop_count)
-    executor.start()
+def bench_10k_add_cdfk():
+    task_count = 10000
     futtable = []
     start = time.perf_counter()
-    for i in range(noop_count):
-        fut = cdfk.submit("xq", "add", time.time(), False, object(), executor, noop)
-        futtable.append(fut)
+    for i in range(task_count):
+        futtable.append(add())
     out = [f.result() for f in futtable]
     end = time.perf_counter()
-    executor.shutdown()
+    dfk.cleanup()
     return end - start
 
 if __name__ == "__main__":
-    for n in [8]:
-        print(f"{n},{50000 / bench_50k_noop_cdfk(n)}")
+    print(f"{10000 / bench_50k_add_cdfk()}")
--- test.py
+++ test.py
@@ -4,35 +4,6 @@ import unittest
 
 from parsl import python_app
 
-"""
-    def test_init_dfk(self):
-        cdfk.init_dfk(10)
-
-    def test_invoke_exec_submit(self):
-        # Uses XQExecutor because this executor does not have a notion
-        # of a provider. Was never sure why it was so challenging to run
-        # the executor detached from the DFk but it seemed as though it
-        # was the provider that made stuff not
-        import time
-        from uuid import uuid4
-        from parsl.executors import XQExecutor
-
-        executor = XQExecutor(
-            max_workers=4,
-        )
-        executor.run_dir = f"parsl_run_dir/{str(uuid4())}"
-        cdfk.init_dfk(10)
-
-        def add():
-            return 1 + 3;
-
-        executor.start()
-        exec_fu = cdfk.submit("xq", "add", time.time(), False, object(), executor, add)
-        while not exec_fu.done():
-            pass
-        executor.shutdown()
-"""
-
 class TestCDFK(unittest.TestCase):
     run_dir = "parsl_run_dir/"
     def test_internal_executor(self):
